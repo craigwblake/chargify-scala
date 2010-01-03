045a1fd5dd6cf6021104b9d97125d6d6a577a403
IntroSpectrum <support@introspectrum.com>
2010-01-03 13:48:18 -0500

    Enabled properties for testing.

--- src/test/scala/com/introspectrum/chargify/ChargifyTest.scala
+++ src/test/scala/com/introspectrum/chargify/ChargifyTest.scala
@@ -1,11 +1,19 @@
 package com.introspectrum.chargify
 
+import java.util.Properties
 import junit.framework._
 
 class ChargifyTest extends TestCase {
 
+	val properties = new Properties()
+	try {
+		properties.load( getClass().getClassLoader().getResourceAsStream( "test.properties"))
+	} catch {
+		case e => throw new RuntimeException( "Please create 'src/test/resources/test.properties' file with 'key' and 'domain' properties.")
+	}
+
 	def testList() = {
-		val chargify = new Chargify( System.getProperty( "key"), System.getProperty( "domain"))
+		val chargify = new Chargify( properties.getProperty( "key"), properties.getProperty( "domain"))
 		val customers = chargify.listCustomers()
 		customers.foreach( println)
 	}
